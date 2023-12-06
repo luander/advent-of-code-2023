f4f49a92a3a3f18588521f8dc8f85d4bbe64e46e
luander <iam@luander.net>
2023-12-06 08:38:19 +0100

    Day 6

--- day-6-wait-for-it/src/part1.rs
+++ day-6-wait-for-it/src/part1.rs
@@ -1,5 +1,31 @@
 fn part1(input: &str) -> u32 {
-    0
+    let time_line = &input.lines().next().unwrap()[9..];
+    let time = time_line
+        .split_whitespace()
+        .filter(|s| !s.is_empty())
+        .map(|s| s.parse().unwrap())
+        .collect::<Vec<u32>>();
+
+    let distance_line = &input.lines().nth(1).unwrap()[9..];
+    let distance = distance_line
+        .split_whitespace()
+        .filter(|s| !s.is_empty())
+        .map(|s| s.parse().unwrap())
+        .collect::<Vec<u32>>();
+
+    let mut result = 1;
+    for i in 0..time.len() {
+        let mut record_beaten = 0;
+        // zero always leads to zero distance
+        for speed in 1..time[i] {
+            let travel_time = time[i] - speed;
+            if (speed * travel_time) > distance[i] {
+                record_beaten += 1;
+            }
+        }
+        result *= record_beaten;
+    }
+    result
 }
 
 pub fn main() {
@@ -14,19 +40,9 @@ mod test {
 
     #[test]
     fn part1_works() {
-        let test_input = "467..114..
-...*......
-..35..633.
-......#...
-617*......
-.....+.58.
-..592.....
-......755.
-...$.*....
-.664.598..
-.*..423.*.";
-
+        let test_input = "Time:      7  15   30
+Distance:  9  40  200";
         let result = part1(test_input);
-        assert_eq!(0, result);
+        assert_eq!(288, result);
     }
 }
--- day-6-wait-for-it/src/part2.rs
+++ day-6-wait-for-it/src/part2.rs
@@ -1,11 +1,40 @@
-fn part1(input: &str) -> u32 {
-    0
+fn part2(input: &str) -> u64 {
+    let time_line = &input.lines().next().unwrap()[9..];
+    let time = String::from(time_line)
+        .replace(' ', "")
+        .parse::<u64>()
+        .unwrap();
+
+    let distance_line = &input.lines().nth(1).unwrap()[9..];
+    let distance = String::from(distance_line)
+        .replace(' ', "")
+        .parse::<u64>()
+        .unwrap();
+
+    let mut from = 0;
+    let mut to = 0;
+    // zero always leads to zero distance
+    for speed in 1..time {
+        let travel_time = time - speed;
+        if (speed * travel_time) > distance {
+            from = speed;
+            break;
+        }
+    }
+    for speed in (1..time).rev() {
+        let travel_time = time - speed;
+        if (speed * travel_time) > distance {
+            to = speed;
+            break;
+        }
+    }
+    to - from + 1
 }
 
 pub fn main() {
     let input1 = include_str!("../input1.txt");
-    let result = part1(input1);
-    println!("part1: {}", result);
+    let result = part2(input1);
+    println!("part2: {}", result);
 }
 
 #[cfg(test)]
@@ -13,20 +42,10 @@ mod test {
     use super::*;
 
     #[test]
-    fn part1_works() {
-        let test_input = "467..114..
-...*......
-..35..633.
-......#...
-617*......
-.....+.58.
-..592.....
-......755.
-...$.*....
-.664.598..
-.*..423.*.";
-
-        let result = part1(test_input);
-        assert_eq!(0, result);
+    fn part2_works() {
+        let test_input = "Time:      7  15   30
+Distance:  9  40  200";
+        let result = part2(test_input);
+        assert_eq!(71503, result);
     }
 }
